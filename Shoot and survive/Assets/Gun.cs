3152ccda960e0a89faa51fe2853537ff45e640f8
DennisBorst <dennis.borst@hotmail.nl>
2019-12-04 20:14:48 +0100

    gun added

---
+++ Shoot and survive/Assets/Gun.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    [SerializeField] private int damage;
+    [SerializeField] private int amountOfBullets;
+    [SerializeField] private float rateOfFire;
+    [SerializeField] private float reloadTime;
+    [SerializeField] private float maxShootDistance;
+    private int currentAmountOfBullets;
+    private float currentReloadTime;
+    private bool isReloading;
+    private float nextTimeToFire = 0f;
+
+    [Header("Weapon attachments")]
+    [SerializeField] private ParticleSystem muzzleFlash;
+    private Camera cam;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAmountOfBullets = amountOfBullets;
+        currentReloadTime = reloadTime;
+
+        anim = GetComponent<Animator>();
+        cam = GetComponentInParent<Camera>();
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        anim.SetBool("Reloading", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Reloading();
+        ShootCheck();
+    }
+
+    private void Reloading()
+    {
+        if (isReloading)
+        {
+            return;
+        }
+
+        if (currentAmountOfBullets <= 0 || Input.GetKeyDown(KeyCode.R) && currentAmountOfBullets != amountOfBullets)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+    }
+
+    private void ShootCheck()
+    {
+        if(!isReloading && Input.GetKey(KeyCode.Mouse0) && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / rateOfFire;
+            currentAmountOfBullets--;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, maxShootDistance))
+        {
+            Health healthScript = hit.transform.GetComponent<Health>();
+
+            if(healthScript != null)
+            {
+                healthScript.TakeDamage(damage);
+            }
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        anim.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - 0.25f);
+        anim.SetBool("Reloading", false);
+        yield return new WaitForSeconds(0.25f);
+
+        currentAmountOfBullets = amountOfBullets;
+        isReloading = false;
+    }
+}
