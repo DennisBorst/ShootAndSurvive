26a11943ea85252fa30909a0e6121fcae5719ab5
Thijs Roem <thijsroem@gmail.com>
2019-12-05 13:33:29 +0100

    enemy update, spawner enemies, spawner pickups, level update

---
+++ Shoot and survive/Assets/Scripts/AIController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AIController : MonoBehaviour
+{    
+    [SerializeField] float movementSpeed = 4f;
+    [SerializeField] float maxDistanceToTarget = 5f;
+    [SerializeField] float timeBetweenAttacks = 1f;
+    [SerializeField] float resetTimeBetweenAttacks;
+    [SerializeField] float damage = 10;
+
+    private GameObject player;
+    private float playerHealth;
+    private Animator anim;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        resetTimeBetweenAttacks = timeBetweenAttacks;
+        player = FindObjectOfType<PlayerController>().gameObject;
+        playerHealth = player.GetComponent<Health>().health;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance > maxDistanceToTarget)
+        {
+            agent.destination = player.transform.position;
+        }
+        else if(distance <= maxDistanceToTarget)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        timeBetweenAttacks -= Time.deltaTime;
+        if(playerHealth > 0)
+        {
+            anim.SetTrigger("Attacking");
+            if (timeBetweenAttacks <= 0)
+            {
+                playerHealth -= damage;
+                timeBetweenAttacks = resetTimeBetweenAttacks;
+            }
+        }
+        print(playerHealth);
+    }
+}
---
+++ Shoot and survive/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using DG.Tweening;
+using TMPro;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public class Wave
+    {
+        public AIController[] enemies;
+        public int count;
+        public float timeBetweenSpawns;
+    }
+
+    public Wave[] waves;
+    public Transform[] spawnPoints;
+    public float timeBetweenWaves;
+
+    private Wave currentWave;
+    private int currentWaveIndex;
+    private Transform objective;
+
+    private bool finishSpawning;
+
+    private void Start()
+    {
+        StartCoroutine(StartNextWave(currentWaveIndex));
+    }
+
+    IEnumerator StartNextWave(int index)
+    {
+        yield return new WaitForSeconds(timeBetweenWaves);
+
+        StartCoroutine(SpawnWave(index));
+    }
+
+    IEnumerator SpawnWave(int index)
+    {
+        currentWave = waves[index];
+
+        for (int i = 0; i < currentWave.count; i++)
+        {
+            if (i == currentWave.count - 1)
+            {
+                finishSpawning = true;
+            }
+            else
+            {
+                finishSpawning = false;
+            }
+
+            AIController randomEnemy = currentWave.enemies[Random.Range(0, currentWave.enemies.Length)];
+            Transform randomSpot = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomEnemy, randomSpot.position, randomSpot.rotation);
+
+            yield return new WaitForSeconds(currentWave.timeBetweenSpawns);
+        }
+    }
+
+    private void Update()
+    {
+        if (finishSpawning == true && GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            finishSpawning = false;
+            if (currentWaveIndex + 1 < waves.Length)
+            {
+                currentWaveIndex++;
+                StartCoroutine(StartNextWave(currentWaveIndex));
+            }
+            else
+            {
+                Debug.Log("Game has finished");
+            }
+        }
+    }
+}
