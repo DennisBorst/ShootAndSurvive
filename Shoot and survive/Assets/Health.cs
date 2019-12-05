d35d401b663b09297f703532c859674c795b7d0a
Thijs Roem <thijsroem@gmail.com>
2019-12-05 10:59:44 +0100

    enemy met neus

--- Shoot and survive/Assets/Health.cs
+++ Shoot and survive/Assets/Health.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private float health;
+    public float health;
 
     // Start is called before the first frame update
     void Start()
